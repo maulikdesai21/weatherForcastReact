4b44ca4d0bb629709f4f3a06ec1b6a8a041276fa
Desai, Maulik (CORP) <Maulik.Desai@adp.com>
2017-10-10 00:56:01 -0400

    Adding Link to Project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '824576e2d5c136e91b323535a71ffc07';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER ='FETCH_WEATHER';
 
 export function fetchWeather(city) {
